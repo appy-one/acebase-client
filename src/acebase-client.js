13aa3d33bd290b34c9cf507884a27079cb30efac
Ewout Stortenbeker <me@appy.one>
2018-11-02 16:41:21 +0100

    Added authentication methods

--- src/acebase-client.js
+++ src/acebase-client.js
@@ -3,9 +3,65 @@ const { WebApi } = require('./api-web');
 //const { EventEmitter } = require('events');
 
 class AceBaseClient extends AceBase {
-    constructor(host, port, dbname) {
-        //TODO: https
-        super(dbname, { api: { class: WebApi, settings: `http://${host}:${port}` } });
+    
+    constructor(host, port, dbname, https = true) {
+        super(dbname, { api: { class: WebApi, settings: `http${https ? 's' : ''}://${host}:${port}` } });
+        this.user = null;
+    }
+
+    signIn(username, password) {
+        /** @type {WebApi} */
+        const api = this.api;
+
+        this.user = null;
+        return api.signIn(username, password)
+        .then(user => {
+            this.user = user;
+            return { success: true, user };
+        })
+        .catch(err => {
+            return { success: false, reason: err.message };
+        });
+    }
+
+    signOut() {
+        /** @type {WebApi} */
+        const api = this.api;
+        return api.signOut()
+        .then(() => {
+            this.user = null;
+            return { success: true };
+        })
+        .catch(err => {
+            return { success: false, reason: err.message };
+        });
+    }
+
+    changePassword(oldPassword, newPassword) {
+        /** @type {WebApi} */
+        const api = this.api;
+        return api.changePassword(this.user.uid, oldPassword, newPassword)
+        .then(() => {
+            return { success: true };
+        })
+        .catch(err => {
+            return { success: false, reason: err.message };
+        });
+    }
+
+    signUp(username, password, displayName) {
+        /** @type {WebApi} */
+        const api = this.api;
+
+        this.user = null;
+        return api.signUp(username, password, displayName)
+        .then(user => {
+            this.user = user;
+            return { success: true, user };
+        })
+        .catch(err => {
+            return { success: false, reason: err.message };
+        });
     }
 }
 

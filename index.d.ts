0d32447f0397dfe348e0936b6d9d58807112cb0e
Ewout Stortenbeker <me@appy.one>
2018-12-14 17:35:49 +0100

    Improved types

--- index.d.ts
+++ index.d.ts
@@ -1,106 +1,110 @@
-declare module "acebase-client" {
+import * as acebasecore from 'acebase-core';
 
-/**
- * AceBaseClient lets you connect to a remote (or local) AceBase server over http(s)
- */
-export class AceBaseClient {
-    constructor(host: string, port: number, dbname: string, https?: boolean);
+declare namespace acebaseclient {
 
     /**
-     * Waits for the database to be ready before running your callback. Do this before performing any other actions on your database
-     * @param {()=>void} [callback] (optional) callback function that is called when ready. You can also use the returned promise
-     * @returns {Promise<void>} returns a promise that resolves when ready
+     * AceBaseClient lets you connect to a remote (or local) AceBase server over http(s)
      */
-    ready(callback: () => void): Promise<void>;
+    class AceBaseClient extends acebasecore.AceBaseBase {
+        constructor(host: string, port: number, dbname: string, https?: boolean);
+
+        /**
+         * Waits for the database to be ready before running your callback. Do this before performing any other actions on your database
+         * @param {()=>void} [callback] (optional) callback function that is called when ready. You can also use the returned promise
+         * @returns {Promise<void>} returns a promise that resolves when ready
+         */
+        ready(callback: () => void): Promise<void>;
+
+        /**
+         * Sign into a user account using a username and password. Note that the server must have authentication enabled.
+         * @param {string} username Your database username
+         * @param {string} password Your password
+         * @returns {Promise<AceBaseSignInResult>} returns a promise that resolves with an object
+         */
+        signIn(username: string, password: string): Promise<AceBaseSignInResult>;
+
+        /**
+         * Sign into an account using a previously assigned access token
+         * @param {string} accessToken a previously assigned access token
+         * @returns {Promise<AceBaseSignInResult>} returns a promise that resolves with an object
+         */
+        signInWithToken(accessToken: string): Promise<AceBaseSignInResult>;
+
+        /**
+         * Signs out of the current account
+         * @returns {Promise<AceBaseAuthResult>} returns a promise that resolves with an object
+         */
+        signOut(): any;
+
+        /**
+         * Changes the password of the currrently signed into account
+         * @param {string} oldPassword
+         * @param {string} newPassword
+         * @returns {Promise<AceBaseAuthResult>} returns a promise that resolves with an object
+         */
+        changePassword(oldPassword: string, newPassword: string): any;
+
+        /**
+         * Creates a new user account with the given details. If successful, you will automatically be
+         * signed into the account. Note: the request will fail if the server has disabled this option
+         * @param {string} username
+         * @param {string} password
+         * @param {string} displayName
+         * @returns {Promise<AceBaseSignInResult>} returns a promise that resolves with an object
+         */
+        signUp(username: string, password: string, displayName: string): any;
+
+    }
+
+    // /**
+    //  * Api to connect to a remote AceBase instance over http
+    //  */
+    // export class WebApi {
+    //     constructor();
+
+    // }
+
+    class AceBaseUser {
+        /**
+         * @param {object} user
+         * @param {string} user.uid
+         * @param {string} user.username
+         */
+        constructor(user: { 
+            uid: string, 
+            username: string 
+        });
+    }
 
     /**
-     * Sign into a user account using a username and password. Note that the server must have authentication enabled.
-     * @param {string} username Your database username
-     * @param {string} password Your password
-     * @returns {Promise<AceBaseSignInResult>} returns a promise that resolves with an object
+     * @param {object} result
+     * @param {boolean} result.success
+     * @param {AceBaseUser} [result.user]
+     * @param {string} [result.accessToken]
+     * @param {string} [result.reason]
      */
-    signIn(username: string, password: string): Promise<AceBaseSignInResult>;
+    class AceBaseSignInResult {
+        constructor(result: {
+            success: boolean;
+            user?: AceBaseUser;
+            accessToken?: string;
+            reason?: string;
+        });
+
+    }
 
     /**
-     * Sign into an account using a previously assigned access token
-     * @param {string} accessToken a previously assigned access token
-     * @returns {Promise<AceBaseSignInResult>} returns a promise that resolves with an object
+     * @param {object} result
+     * @param {boolean} result.success
+     * @param {string} [result.reason]
      */
-    signInWithToken(accessToken: string): Promise<AceBaseSignInResult>;
+    class AceBaseAuthResult {
+        constructor(result: {
+            success: boolean;
+            reason?: string;
+        });
 
-    /**
-     * Signs out of the current account
-     * @returns {Promise<AceBaseAuthResult>} returns a promise that resolves with an object
-     */
-    signOut(): any;
-
-    /**
-     * Changes the password of the currrently signed into account
-     * @param {string} oldPassword
-     * @param {string} newPassword
-     * @returns {Promise<AceBaseAuthResult>} returns a promise that resolves with an object
-     */
-    changePassword(oldPassword: string, newPassword: string): any;
-
-    /**
-     * Creates a new user account with the given details. If successful, you will automatically be
-     * signed into the account. Note: the request will fail if the server has disabled this option
-     * @param {string} username
-     * @param {string} password
-     * @param {string} displayName
-     * @returns {Promise<AceBaseSignInResult>} returns a promise that resolves with an object
-     */
-    signUp(username: string, password: string, displayName: string): any;
-
+    }
 }
 
-// /**
-//  * Api to connect to a remote AceBase instance over http
-//  */
-// export class WebApi {
-//     constructor();
-
-// }
-
-export class AceBaseUser {
-    /**
-     * @param {object} user
-     * @param {string} user.uid
-     * @param {string} user.username
-     */
-    constructor(user: { 
-        uid: string, 
-        username: string 
-    });
-}
-
-/**
- * @param {object} result
- * @param {boolean} result.success
- * @param {AceBaseUser} [result.user]
- * @param {string} [result.accessToken]
- * @param {string} [result.reason]
- */
-export class AceBaseSignInResult {
-    constructor(result: {
-        success: boolean;
-        user?: AceBaseUser;
-        accessToken?: string;
-        reason?: string;
-    });
-
-}
-
-/**
- * @param {object} result
- * @param {boolean} result.success
- * @param {string} [result.reason]
- */
-export class AceBaseAuthResult {
-    constructor(result: {
-        success: boolean;
-        reason?: string;
-    });
-
-}
-}
+export = acebaseclient;

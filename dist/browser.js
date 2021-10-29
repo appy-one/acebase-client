d261cbaf1d6590c3e0a767395c6fc58477b33df8
Ewout Stortenbeker <4ewout@gmail.com>
2021-10-29 15:49:33 +0200

    New browser script include bundles

--- dist/browser.js
+++ dist/browser.js
@@ -5130,7 +5130,7 @@ class DataReferenceQuery {
      * Executes the query and returns if there are any results
      */
     exists() {
-        return this.count().then(count => count > 1);
+        return this.count().then(count => count > 0);
     }
     /**
      * Executes the query, removes all matches from the database

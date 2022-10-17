c65d6bed573c01ac151597bf5c70b1ad0fa7d011
Ewout Stortenbeker <me@appy.one>
2022-10-17 15:59:33 +0200

    eslint fixes, TODO use generated types in /dist

--- index.d.ts
+++ index.d.ts
@@ -2,23 +2,23 @@ import * as acebasecore from 'acebase-core';
 
 export interface AceBaseClientCacheSettings {
     /**
-     * AceBase database to use as local cache. Any data loaded from the server is 
+     * AceBase database to use as local cache. Any data loaded from the server is
      * automatically cached and become available offline. Any changes you make will
-     * update both the server and the cache. When offline, all changes will be 
+     * update both the server and the cache. When offline, all changes will be
      * synchronized with the server upon reconnect.
      */
     db: acebasecore.AceBaseBase,
     /**
-     * Which database to use as primary target for getting and updating data. 
-     * 
+     * Which database to use as primary target for getting and updating data.
+     *
      * Using 'server' (default) is recommended.
-     *  
-     * Using 'cache' will be faster, but has some disadvantages: 
-     * - When getting values, cache is not updated with server data so any remote changes 
-     *    will not be updated in cache unless you have change events setup, or fetch fresh 
+     *
+     * Using 'cache' will be faster, but has some disadvantages:
+     * - When getting values, cache is not updated with server data so any remote changes
+     *    will not be updated in cache unless you have change events setup, or fetch fresh
      *    data manually.
      * - When storing values, you won't know if the server update failed.
-     * 
+     *
      * Summary: use 'server' unless you know what you're doing.
      * @default 'server'
      */
@@ -31,15 +31,15 @@ export interface AceBaseClientCacheSettings {
 }
 export interface AceBaseClientSyncSettings {
     /**
-     * Determines when synchronization should execute: 
-     * 
+     * Determines when synchronization should execute:
+     *
      * - after `"connect"` event
      * - after `"signin"` event
-     * - `"manual"` with `client.sync()`, or 
+     * - `"manual"` with `client.sync()`, or
      * - `"auto"`, which is 2.5s after `"connect"` event, or immediately after `"signin"` event. (legacy behaviour)
-     * 
-     * If your app needs to sync data that is only accessible to the signed in user, set this 
-     * to `"signin"`. If not, set this to `"connect"`. The `"auto"` setting is default and provided 
+     *
+     * If your app needs to sync data that is only accessible to the signed in user, set this
+     * to `"signin"`. If not, set this to `"connect"`. The `"auto"` setting is default and provided
      * for backward compatibility, but should not be used anymore. If you want to manually
      * trigger synchronization with `client.sync()`, set this to `"manual"`
      * @default 'auto'
@@ -47,7 +47,7 @@ export interface AceBaseClientSyncSettings {
     timing?: 'connect'|'signin'|'auto'|'manual';
 
     /**
-     * Specifies whether to use cursor synchronization if transaction logging is enabled in the server configuration. 
+     * Specifies whether to use cursor synchronization if transaction logging is enabled in the server configuration.
      * Synchronization with a cursor is faster and consumes (a lot) less bandwidth
      * @default true
      */
@@ -84,15 +84,15 @@ export interface AceBaseClientConnectionSettings {
      */
     network?: {
         /**
-         * Whether to actively monitor the network, checks connectivity with the server every `interval` seconds. 
-         * NOTE: disconnects to the server are discovered automatically under normal circumstances, 
+         * Whether to actively monitor the network, checks connectivity with the server every `interval` seconds.
+         * NOTE: disconnects to the server are discovered automatically under normal circumstances,
          * enabling this might cause disconnects to be detected earlier.
-         * 
+         *
          * Default is `false` if `realtime` is `true` (default) and vice versa
          */
         monitor?: boolean
         /**
-         * Perform conncetivity check every `interval` seconds. 
+         * Perform conncetivity check every `interval` seconds.
          * @default 60
          */
         interval?: number
@@ -101,7 +101,7 @@ export interface AceBaseClientConnectionSettings {
          */
         transports?: Array<'polling'|'websocket'>
         /**
-         * Whether to connect to a serverwebsocket to enable realtime event notifications. 
+         * Whether to connect to a serverwebsocket to enable realtime event notifications.
          * Default is `true`. Disable this option if you only want to use the server's REST API.
          */
         realtime?: boolean
@@ -120,8 +120,8 @@ export class AceBaseClient extends acebasecore.AceBaseBase {
      * @deprecated use new constructor
      */
     constructor(host: string, port: number, dbname: string, https?: boolean);
-    readonly auth: AceBaseClientAuth
-    readonly connected: boolean
+    readonly auth: AceBaseClientAuth;
+    readonly connected: boolean;
     readonly connectionState: 'disconnected'|'connecting'|'connected'|'disconnecting';
 
     /**
@@ -151,11 +151,11 @@ export class AceBaseClient extends acebasecore.AceBaseBase {
      * Initiates manual synchronization with the server of any paths with active event subscriptions. Use this if you have set the `sync.timing` connection setting to 'manual'
      */
     sync(): Promise<void>
-    
+
     readonly cache: {
         /**
          * Clears the entire cache, or a specific path without raising any events
-         * @param path 
+         * @param path
          */
         clear(path?: string): Promise<void>
 
@@ -169,33 +169,33 @@ export class AceBaseClient extends acebasecore.AceBaseBase {
         update(path?: string, cursor?: string|null): Promise<{ path: string, used_cursor: string, new_cursor: string, loaded_value: boolean, changes: Array<{ path: string, previous: any, value: any, context: any }> }>
 
         /**
-         * Loads a value from cache database.If a cursor is provided, the cache will be updated with remote changes 
+         * Loads a value from cache database.If a cursor is provided, the cache will be updated with remote changes
          * first. If the value is not available in cache, it will be loaded from the server and stored in cache.
-         * This method is a shortcut for common cache logic provided by `db.ref(path).get` with the `cache_mode` 
+         * This method is a shortcut for common cache logic provided by `db.ref(path).get` with the `cache_mode`
          * and `cache_cursor` options.
          * @param path target path to load
          * @param cursor A previously acquired cursor
          * @returns Returns a Promise that resolves with a snapshot of the value
          */
-         get(path: string, cursor?: string): Promise<DataSnapshot>        
-    }
+         get(path: string, cursor?: string): Promise<DataSnapshot>
+    };
 
 }
 
-export interface IAceBaseAuthProviderSignInResult { 
-    user: AceBaseUser, 
-    accessToken: string, 
-    provider: IAceBaseAuthProviderTokens 
+export interface IAceBaseAuthProviderSignInResult {
+    user: AceBaseUser,
+    accessToken: string,
+    provider: IAceBaseAuthProviderTokens
 }
-export interface IAceBaseAuthProviderTokens { 
-    name: string, 
-    access_token: string, 
-    refresh_token: string, 
-    expires_in: number 
+export interface IAceBaseAuthProviderTokens {
+    name: string,
+    access_token: string,
+    refresh_token: string,
+    expires_in: number
 }
 export class AceBaseClientAuth {
-    user?: AceBaseUser
-    accessToken: string
+    user?: AceBaseUser;
+    accessToken: string;
 
     /**
      * Sign into a user account using a username and password. Note that the server must have authentication enabled.
@@ -221,7 +221,7 @@ export class AceBaseClientAuth {
     signInWithToken(accessToken: string): Promise<{ user: AceBaseUser, accessToken: string }>;
 
     /**
-     * If the client is offline, you can specify an access token to automatically try signing in the user once a connection is made. 
+     * If the client is offline, you can specify an access token to automatically try signing in the user once a connection is made.
      * Doing this is recommended if you are subscribing to event paths that require user authentication/authorization. Subscribing to
      * those server events will then be done after signing in, instead of failing after connecting anonymously.
      * @param accessToken A previously acquired access token
@@ -230,7 +230,7 @@ export class AceBaseClientAuth {
 
     /**
      * If the server has been configured with OAuth providers, use this to kick off the authentication flow.
-     * This method returs a Promise that resolves with the url you have to redirect your user to authenticate 
+     * This method returs a Promise that resolves with the url you have to redirect your user to authenticate
      * with the requested provider. After the user has authenticated, they will be redirected back to your callbackUrl.
      * Your code in the callbackUrl will have to call finishOAuthProviderSignIn with the result querystring parameter
      * to finish signing in.
@@ -258,12 +258,12 @@ export class AceBaseClientAuth {
      * Signs in with an external auth provider by redirecting the user to the provider's login page.
      * After signing in, the user will be redirected to the current browser url. Execute
      * getRedirectResult() when your page is loaded again to check if the user was authenticated.
-     * @param {string} providerName 
+     * @param {string} providerName
      */
     signInWithRedirect(providerName: string): void;
 
-    /** 
-     * Checks if the user authentication with an auth provider. 
+    /**
+     * Checks if the user authentication with an auth provider.
      */
     getRedirectResult(): Promise<IAceBaseAuthProviderSignInResult>;
 
@@ -285,7 +285,7 @@ export class AceBaseClientAuth {
      * @param {boolean} everywhere whether to sign out all clients, or only this one
      */
     signOut(everywhere: boolean): Promise<void>;
-    /** 
+    /**
      * @param {any} options
      * @param {boolean} [options.everywhere=false] whether to sign out all clients, or only this one
      * @param {boolean} [options.clearCache=false] if cache database is used: whether to clear cached data (recommended, currently not enabled by default, might change in next major version)
@@ -293,7 +293,7 @@ export class AceBaseClientAuth {
     signOut(options: { everywhere?: boolean, clearCache?: boolean }): Promise<void>;
 
     /**
-     * Changes the password of the currrently signed into account
+     * Changes the password of the currently signed into account
      * @param {string} oldPassword
      * @param {string} newPassword
      * @returns {Promise<{ accessToken: string }>} returns a promise that resolves with a new access token
@@ -306,7 +306,7 @@ export class AceBaseClientAuth {
      * @returns returns a promise that resolves once the request has been processed
      */
     forgotPassword(email: string): Promise<void>;
-    
+
     /**
      * Requests a password to be changed using a previously acquired reset code, sent to the email address with forgotPassword
      * @param {string} resetCode
@@ -319,15 +319,15 @@ export class AceBaseClientAuth {
      * Creates a new user account with the given details. If successful, you will automatically be
      * signed into the account. Note: the request will fail if the server has disabled this option
      * @param {object} details
-     * @param {string} [details.username] 
-     * @param {string} [details.email] 
+     * @param {string} [details.username]
+     * @param {string} [details.email]
      * @param {string} details.password
      * @param {string} details.displayName
-     * @param {{ [key:string]: string|number|boolean }} [details.settings] optional settings 
+     * @param {{ [key:string]: string|number|boolean }} [details.settings] optional settings
      * @returns {Promise<{ user: AceBaseUser, accessToken: string }>} returns a promise that resolves with the signed in user and access token
      */
     signUp(details: { username?: string, email?: string, password: string, displayName: string, settings?: { [key:string]: string|number|boolean } }): Promise<{ user: AceBaseUser, accessToken: string }>;
-    
+
     /**
      * Verifies an e-mail address using the code sent to the email address upon signing up
      * @param {string} verificationCode
@@ -344,21 +344,21 @@ export class AceBaseClientAuth {
 
     /**
      * Changes the username of the currrently signed into account
-     * @param {string} newUsername 
+     * @param {string} newUsername
      * @returns {Promise<{ user: AceBaseUser }>} returns a promise that resolves with the updated user details
      */
     changeUsername(newUsername: string): Promise<{ user: AceBaseUser }>
 
     /**
      * Changes the display name of the currrently signed into account
-     * @param {string} newName 
+     * @param {string} newName
      * @returns {Promise<{ user: AceBaseUser }>} returns a promise that resolves with the updated user details
      */
     changeDisplayName(newName: string): Promise<{ user: AceBaseUser }>
 
     /**
      * Changes the email address of the currrently signed in user
-     * @param {string} newEmail 
+     * @param {string} newEmail
      * @returns {Promise<{ user: AceBaseUser }>} returns a promise that resolves with the updated user details
      */
     changeEmail(newEmail: string): Promise<{ user: AceBaseUser }>
@@ -366,7 +366,7 @@ export class AceBaseClientAuth {
     /**
      * Changes the user's profile picture
      * @returns returns a promise that resolves with the updated user details
-     */    
+     */
     changePicture(newPicture: { url: string, width: number, height: number }): Promise<{ user: AceBaseUser }>
 
     /**
@@ -388,21 +388,21 @@ export class AceBaseClientAuth {
 }
 
 export class AceBaseUser {
-    uid: string
-    username?: string
-    email?: string
-    displayName?: string
-    settings: { [key:string]: string|number|boolean }
-    created: string //Date
-    createdIp: string
-    lastSignin: string //Date
-    lastSigninIp: string
-    prevSignin?: string //Date
-    prevSigninIp?: string
-    changePassword?: boolean
-    changePasswordRequested?: string
-    changePasswordBefore?: string //Date
-    picture?: { width: number, height: number, url }
+    uid: string;
+    username?: string;
+    email?: string;
+    displayName?: string;
+    settings: { [key:string]: string|number|boolean };
+    created: string; //Date
+    createdIp: string;
+    lastSignin: string; //Date
+    lastSigninIp: string;
+    prevSignin?: string; //Date
+    prevSigninIp?: string;
+    changePassword?: boolean;
+    changePasswordRequested?: string;
+    changePasswordBefore?: string; //Date
+    picture?: { width: number, height: number, url };
 
     constructor(user: AceBaseUser);
 }
@@ -445,7 +445,7 @@ export class AceBaseUser {
 
 export class ServerDate extends Date {}
 export class CachedValueUnavailableError extends Error {
-    path: string
+    path: string;
 }
 
 export import DataSnapshot = acebasecore.DataSnapshot;

70e3399d05a6fc588062fd0befdc4f3e466bd084
Ewout Stortenbeker <me@appy.one>
2022-10-24 15:06:58 +0200

    added headers, export all current exports

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,62 @@
-export { DataReference, DataSnapshot, EventSubscription, PathReference, TypeMappings, ID, proxyAccess, ObjectCollection, PartialArray, Transport } from 'acebase-core';
-export { AceBaseClient } from './acebase-client';
+/**
+   ________________________________________________________________________________
+
+      ___          ______
+     / _ \         | ___ \
+    / /_\ \ ___ ___| |_/ / __ _ ___  ___
+    |  _  |/ __/ _ \ ___ \/ _` / __|/ _ \
+    | | | | (_|  __/ |_/ / (_| \__ \  __/
+    \_| |_/\___\___\____/ \__,_|___/\___|
+                 realtime database client
+
+   Copyright 2018-2022 by Ewout Stortenbeker <http://appy.one>
+   Released under MIT license
+
+   See docs at https://github.com/appy-one/acebase-client
+   ________________________________________________________________________________
+
+*/
+/*!
+ * AceBaseClient
+ * (c) 2018-2022 Ewout Stortenbeker <http://appy.one>
+ * Released under MIT license
+ */
+export {
+    AceBaseClient,
+    AceBaseClientConnectionSettings,
+    AceBaseClientCacheSettings,
+    AceBaseClientSyncSettings,
+} from './acebase-client';
+
+export {
+    IAceBaseAuthProviderSignInResult,
+    IAceBaseAuthProviderTokens,
+} from './api-web';
+
+export { AceBaseClientAuth } from './auth';
+export { AceBaseUser } from './user';
 export { ServerDate } from './server-date';
 export { CachedValueUnavailableError } from './errors';
+
+export {
+    DataReference,
+    DataSnapshot,
+    DataSnapshotsArray,
+    DataReferencesArray,
+    EventStream,
+    EventSubscription,
+    PathReference,
+    TypeMappings,
+    TypeMappingOptions,
+    IReflectionNodeInfo,
+    IReflectionChildrenInfo,
+    IStreamLike,
+    ILiveDataProxy,
+    ILiveDataProxyValue,
+    IObjectCollection,
+    ObjectCollection,
+    ID,
+    proxyAccess,
+    PartialArray,
+    // Transport, // No longer export, has never been publically typed
+} from 'acebase-core';

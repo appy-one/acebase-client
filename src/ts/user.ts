91c358bf96cf6b9b7333a9d1e812dceb6aa10548
Ewout Stortenbeker <me@appy.one>
2022-10-24 15:05:34 +0200

    removed unused types

--- src/ts/user.ts
+++ src/ts/user.ts
@@ -37,7 +37,7 @@ export class AceBaseUser {
     /**
      * Additional saved user settings & info
      */
-    settings: { [key:string]: string|number|boolean };
+    settings: { [key:string]: string | number | boolean };
 
     constructor(user: Partial<AceBaseUser>) {
         Object.assign(this, user);
@@ -48,38 +48,3 @@ export class AceBaseUser {
         this.settings = user.settings ?? {};
     }
 }
-
-export type AceBaseSignInSuccess = { success: true; user: AceBaseUser; accessToken: string; provider: string };
-export type AceBaseSignInFailure = { success: false; reason: { code: string, message: string }; };
-export type AceBaseSignInResult = AceBaseSignInSuccess | AceBaseSignInFailure;
-// export class AceBaseSignInResult {
-//     success: boolean;
-//     user?: AceBaseUser;
-//     accessToken?: string;
-//     reason?: { code: string, message: string };
-//     constructor(result: { success: boolean; user?: AceBaseUser; accessToken?: string; reason?: { code: string, message: string } }) {
-//         this.success = result.success;
-//         if (result.success) {
-//             this.user = result.user;
-//             this.accessToken = result.accessToken;
-//         }
-//         else {
-//             this.reason = result.reason;
-//         }
-//     }
-// }
-
-export type AceBaseAuthSuccess = { success: true };
-export type AceBaseAuthFailure = { success: false; reason: { code: string, message: string } };
-export type AceBaseAuthResult = AceBaseAuthSuccess | AceBaseAuthFailure;
-
-// export class AceBaseAuthResult {
-//     success: boolean;
-//     reason?: { code: string, message: string };
-//     constructor(result: { success: boolean; reason?: { code: string, message: string }}) {
-//         this.success = result.success;
-//         if (!result.success) {
-//             this.reason = result.reason;
-//         }
-//     }
-// }

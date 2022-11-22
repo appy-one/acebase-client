39e32ea7eae64f0bdf07ad7f17f2152d39955c70
Ewout Stortenbeker <me@appy.one>
2022-11-22 21:25:57 +0100

    add tests - simple starter

---
+++ test/README.md
@@ -0,0 +1,6 @@
+## Tests
+
+Follow these steps to test:
+* Install the dependencies with `npm install`.
+* Build with `npm run build`
+* Run the tests with `npm test`
---
+++ test/src/run.ts
@@ -0,0 +1,62 @@
+import { AceBaseServer } from 'acebase-server';
+import { AceBaseClient } from '../../dist/types';
+import { settings } from './settings.js';
+
+async function startServer(dbname: string) {
+    const server = new AceBaseServer(dbname, { port: settings.port, authentication: { enabled: true, defaultAdminPassword: settings.password } });
+    await server.ready();
+    return server;
+}
+
+export async function run(modules: 'ESM'|'CommonJS', Client: typeof AceBaseClient) {
+    const dbname = `test-${modules}`;
+
+    // Start server
+    const server = await startServer(dbname);
+
+    // Connect
+    const db = new Client({ dbname, host: 'localhost', port: settings.port, https: false });
+    console.log(`${modules} module load test succeeded`);
+    await db.ready();
+    console.log('Connected');
+
+    // Disconnect
+    db.disconnect();
+
+    // Reconnect
+    await db.connect();
+
+    // Sign in as admin
+    let user = await db.auth.signIn('admin', settings.password);
+    console.log('Signed in as admin', user);
+
+    // Sign out
+    await db.auth.signOut();
+
+    // Sign in with token
+    user = await db.auth.signInWithToken(user.accessToken);
+    console.log('Signed in with token', user);
+
+    // Pause server
+    await server.pause();
+
+    // TODO: Expect to be disconnected
+
+    // Resume server
+    await server.resume();
+
+    // TODO: Expect to reconnect
+
+    // Store test data
+    await db.ref('test').set({
+        text: `Storing this text from ${modules} tests`,
+    });
+
+    // Disconnect
+    db.disconnect();
+
+    // Stop server
+    server.shutdown();
+
+    // TODO: Expect automatic app exit now
+}

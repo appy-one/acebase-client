b89744edbe20059d042d382b31442356c1b39110
Ewout Stortenbeker <me@appy.one>
2022-12-12 16:31:29 +0100

    re-add comment

--- src/acebase-client.ts
+++ src/acebase-client.ts
@@ -406,6 +406,7 @@ export class AceBaseClient extends AceBaseBase {
 
     /**
      * Manually connects to the server: use this if you have `autoConnect` disabled in your client config
+     * @param retry Whether to keep retrying to connect if the connection fails. Default is `true`
      */
     connect(retry = true) {
         return this.api.connect(retry);

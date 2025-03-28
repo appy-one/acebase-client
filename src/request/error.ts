994c78a9cfbe7f4f4b4c58271fee89515bdd256c
Ewout Stortenbeker <me@appy.one>
2025-03-28 10:22:55 +0100

    Add network/server related error codes to `isNetworkError` getter, add `isPermissionError` (#67)
    * Add network/server related error codes to `isNetworkError` getter, add `isPermissionError`
    
    * Include `response.statusCode` in `isNetworkError` check
    
    * Let `isPermissionError` also check `response.statusCode`

--- src/request/error.ts
+++ src/request/error.ts
@@ -1,6 +1,11 @@
 export class AceBaseRequestError extends Error {
     get isNetworkError() {
-        return this.response === null;
+        // 408: Request Timeout, 429: Too Many Requests, 500: Internal Server Error, 502: Bad Gateway, 503: Service Unavailable, 504: Gateway Timeout
+        return this.response === null || [408, 429, 500, 502, 503, 504].some((code) => [this.response?.statusCode, this.code].includes(code));
+    }
+    get isPermissionError() {
+        // 401: Unauthorized, 403: Forbidden
+        return this.response !== null && ([401, 403]).some((code) => [this.response?.statusCode, this.code].includes(code));
     }
     constructor(public request: any, public response: any, public code?: number | string, public message: string = 'unknown error') {
         super(message);

4ee1d546e3556131c622f4246c15a983039dc5db
Ewout Stortenbeker <me@appy.one>
2025-03-28 09:24:32 +0100

    Include `response.statusCode` in `isNetworkError` check

--- src/request/error.ts
+++ src/request/error.ts
@@ -1,7 +1,7 @@
 export class AceBaseRequestError extends Error {
     get isNetworkError() {
         // 408: Request Timeout, 429: Too Many Requests, 500: Internal Server Error, 502: Bad Gateway, 503: Service Unavailable, 504: Gateway Timeout
-        return this.response === null || [408, 429, 500, 502, 503, 504].includes(this.code as number);
+        return this.response === null || [408, 429, 500, 502, 503, 504].some((code) => [this.response?.statusCode, this.code].includes(code));
     }
     get isPermissionError() {
         // 401: Unauthorized, 403: Forbidden
